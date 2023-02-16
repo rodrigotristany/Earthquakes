43a57514e93ed21e5d7cbcd43a8f04d8fc5fdf55
Rodrigo Tristany <rodrigo@snowshoestamp.com>
2023-02-16 16:11:42 -0300

    Initial Commit

---
+++ Earthquakes/EarthquakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthquakesApp.swift
+//  Earthquakes
+//
+//  Created by Rodrigo Tristany on 16/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct EarthquakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
