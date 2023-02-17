1eda999e834ea9c11c4970a6ad38f6f1ab71b225
Rodrigo Tristany <rodrigo@snowshoestamp.com>
2023-02-17 10:01:28 -0300

    Lesson: Caching network data

---
+++ Earthquakes/Models/NSCache+Subscript.swift
@@ -0,0 +1,28 @@
+//
+//  NSCache+Subscript.swift
+//  Earthquakes
+//
+//  Created by Rodrigo Tristany on 17/02/2023.
+//
+
+import Foundation
+
+extension NSCache where KeyType == NSString, ObjectType == CacheEntryObject {
+    subscript(_ url: URL) -> CacheEntry? {
+        get {
+            let key = url.absoluteString as NSString
+            let value = object(forKey: key)
+            return value?.entry
+        }
+        set {
+            let key = url.absoluteString as NSString
+            if let entry = newValue {
+                let value = CacheEntryObject(entry: entry)
+                setObject(value, forKey: key)
+            }
+            else {
+                removeObject(forKey: key)
+            }
+        }
+    }
+}
--- Earthquakes/Models/QuakeClient.swift
+++ Earthquakes/Models/QuakeClient.swift
@@ -7,16 +7,9 @@
 
 import Foundation
 
-class QuakeClient {
+actor QuakeClient {
+    private let quakeCache: NSCache<NSString, CacheEntryObject> = NSCache()
 
-    var quakes: [Quake] {
-        get async throws {
-            let data = try await downloader.httpData(from: feedURL)
-            let allQuakes = try decoder.decode(GeoJSON.self, from: data)
-            return allQuakes.quakes
-        }
-    }
-    
     private lazy var decoder: JSONDecoder = {
         let aDecoder = JSONDecoder()
         aDecoder.dateDecodingStrategy = .millisecondsSince1970
@@ -27,7 +20,60 @@ class QuakeClient {
 
     private let downloader: any HTTPDataDownloader
     
+    var quakes: [Quake] {
+        get async throws {
+            let data = try await downloader.httpData(from: feedURL)
+            let allQuakes = try decoder.decode(GeoJSON.self, from: data)
+            var updatedQuakes = allQuakes.quakes
+            if let olderThanOneHour = updatedQuakes.firstIndex(where: {  $0.time.timeIntervalSinceNow > 3600 }) {
+                let indexRange = updatedQuakes.startIndex..<olderThanOneHour
+                try await withThrowingTaskGroup(of: (Int, QuakeLocation).self) { group in
+                    for index in indexRange {
+                        group.addTask {
+                            let location = try await self.quakeLocation(from: allQuakes.quakes[index].detail)
+                            return (index, location)
+                        }
+                    }
+                    while let result = await group.nextResult() {
+                        switch result {
+                        case .failure(let error):
+                            throw error
+                        case .success(let (index, location)):
+                            updatedQuakes[index].location = location
+                        }
+                    }
+                }
+            }
+            return updatedQuakes
+        }
+    }
+    
     init(downloader: any HTTPDataDownloader = URLSession.shared) {
         self.downloader = downloader
+    }
+    
+    func quakeLocation(from url: URL) async throws -> QuakeLocation {
+        if let cached = quakeCache[url] {
+            switch cached {
+            case .ready(let location):
+                return location
+            case .inProgress(let task):
+                return try await task.value
+            }
+        }
+        let task = Task<QuakeLocation, Error> {
+            let data = try await downloader.httpData(from: url)
+            let location = try decoder.decode(QuakeLocation.self, from: data)
+            return location
+        }
+        quakeCache[url] = .inProgress(task)
+        do {
+            let location = try await task.value
+            quakeCache[url] = .ready(location)
+            return location
+        } catch {
+            quakeCache[url] = nil
+            throw error
+        }
     }
 }
